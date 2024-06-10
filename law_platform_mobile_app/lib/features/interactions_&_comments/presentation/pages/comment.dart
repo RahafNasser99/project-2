6246e7baffc00593bc245876f885758629ca3a1c
RahafNasser99 <90472612+RahafNasser99@users.noreply.github.com>
2024-06-10 15:37:54 +0300

    add comment presentation

--- law_platform_mobile_app/lib/features/interactions_&_comments/presentation/pages/comment.dart
+++ law_platform_mobile_app/lib/features/interactions_&_comments/presentation/pages/comment.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:law_platform_mobile_app/features/interactions_&_comments/presentation/widgets/add_comment_widget.dart';
 import 'package:law_platform_mobile_app/features/interactions_&_comments/presentation/widgets/comment_widget.dart';
+import 'package:keyboard_height_plugin/keyboard_height_plugin.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 
-class CommentsPage extends StatelessWidget {
+class CommentsPage extends StatefulWidget {
   const CommentsPage({super.key});
+
+  @override
+  State<CommentsPage> createState() => _CommentsPageState();
+}
+
+class _CommentsPageState extends State<CommentsPage> {
+  double _keyboardHeight = 0;
+  double actualHeight = 0;
+
+  final KeyboardHeightPlugin _keyboardHeightPlugin = KeyboardHeightPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    _keyboardHeightPlugin.onKeyboardHeightChanged((double height) {
+      setState(() {
+        _keyboardHeight = height;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
-    
+    final statusBarHeight = MediaQuery.of(context).viewPadding.top;
+
     AppBar appBar = AppBar(
       title: Text(
         'التعليقات',
@@ -18,26 +41,49 @@ class CommentsPage extends StatelessWidget {
     );
     return Scaffold(
       appBar: appBar,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          SizedBox(
-            height: (height - appBar.preferredSize.height - 24) * 0.9,
-            child: ListView.separated(
-              itemCount: 10,
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              itemBuilder: (context, index) => const CommentWidget(),
-              separatorBuilder: (context, index) => const SizedBox(
-                height: 16.0,
+      body: KeyboardVisibilityBuilder(
+        builder: (context, isKeyboardVisible) {
+          actualHeight = height -
+              _keyboardHeight -
+              appBar.preferredSize.height -
+              statusBarHeight;
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              SizedBox(
+                height:
+                    isKeyboardVisible ? actualHeight * 0.6 : actualHeight * 0.8,
+                child: ListView.separated(
+                  itemCount: 10,
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  itemBuilder: (context, index) => const CommentWidget(),
+                  separatorBuilder: (context, index) => const SizedBox(
+                    height: 16.0,
+                  ),
+                ),
               ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-            height: (height - appBar.preferredSize.height - 24) * 0.1,
-            child: const AddCommentWidget(),
-          ),
-        ],
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+                height:
+                    isKeyboardVisible ? actualHeight * 0.4 : actualHeight * 0.2,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                  boxShadow: [
+                    BoxShadow(
+                      spreadRadius: 2.0,
+                      blurRadius: 2.0,
+                      color: Theme.of(context).colorScheme.surface,
+                    )
+                  ],
+                ),
+                child: AddCommentWidget(
+                  isKeyboardVisible: isKeyboardVisible,
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
--- law_platform_mobile_app/lib/features/interactions_&_comments/presentation/widgets/add_comment_widget.dart
+++ law_platform_mobile_app/lib/features/interactions_&_comments/presentation/widgets/add_comment_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AddCommentWidget extends StatefulWidget {
-  const AddCommentWidget({super.key});
+  const AddCommentWidget({super.key, required this.isKeyboardVisible});
+
+  final bool isKeyboardVisible;
 
   @override
   State<AddCommentWidget> createState() => _AddCommentWidgetState();
@@ -9,11 +11,20 @@ class AddCommentWidget extends StatefulWidget {
 
 class _AddCommentWidgetState extends State<AddCommentWidget> {
   final _formKey = GlobalKey<FormState>();
+  final TextEditingController _textEditingController = TextEditingController();
+  String _commentText = '';
 
   Future<void> _submit() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      print(_commentText);
     }
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
   }
 
   @override
@@ -21,27 +32,41 @@ class _AddCommentWidgetState extends State<AddCommentWidget> {
     final width = MediaQuery.of(context).size.width - 16;
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         SizedBox(
           width: width * 0.9,
           child: Form(
             key: _formKey,
-            child: TextFormField(
+            child: TextField(
               maxLines: null,
+              textAlign: TextAlign.right,
+              textDirection: TextDirection.rtl,
+              textInputAction: TextInputAction.newline,
+              controller: _textEditingController,
+              style: Theme.of(context).textTheme.bodyLarge,
+              cursorHeight: Theme.of(context).textTheme.bodyLarge!.fontSize,
               decoration: InputDecoration(
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25.0)),
+                contentPadding: widget.isKeyboardVisible
+                    ? const EdgeInsets.symmetric(
+                        horizontal: 12.0, vertical: 2.0)
+                    : const EdgeInsets.symmetric(
+                        horizontal: 12.0, vertical: 0.0),
+                border: InputBorder.none,
                 hintText: 'أضف تعليقك',
                 hintTextDirection: TextDirection.rtl,
                 hintStyle: Theme.of(context).textTheme.labelLarge,
               ),
+              onChanged: (value) {
+                _commentText = value;
+              },
             ),
           ),
         ),
         SizedBox(
           width: width * 0.1,
           child: IconButton(
-            onPressed: () {},
+            onPressed: _submit,
             icon: const Icon(Icons.reply_rounded),
           ),
         )
